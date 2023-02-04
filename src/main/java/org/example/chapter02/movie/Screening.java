2934d04b7996adf630bfe479fec968f49a664fc2
birdieHyun <flaehdan@gmail.com>
2023-02-04 19:39:51 +0900

    feat : chapter02

---
+++ src/main/java/org/example/chapter02/movie/Screening.java
@@ -0,0 +1,35 @@
+package org.example.chapter02.movie;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public LocalDateTime getStartTime() {
+        return whenScreened;
+    }
+
+    public boolean isSequence(int sequence) {
+        return this.sequence == sequence;
+    }
+
+    public Movey getMovieFee() {
+        return movie.getFee();
+    }
+
+    public Reservation reserve(Customer customer, int audiencCount) {
+        return new Reservation(customer, this, calculateFee(audiencCount), audiencCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+}
